7a932f8106a3d7f3cc0f40f21ca67cfa2ab2df59
TrixiS <oficialmorozov@gmail.com>
2022-11-14 13:05:47 +0300

    Add di

---
+++ src/decorators.ts
@@ -0,0 +1,15 @@
+import { Container } from "./container";
+
+export const Injectable = (): ClassDecorator => {
+  return (targetConstructor) => {
+    Container.instance.set(targetConstructor as any);
+  };
+};
+
+export const Inject = (): PropertyDecorator => {
+  return (target, propertyKey) => {
+    const id = (Reflect as any).getMetadata("design:type", target, propertyKey);
+    const injectable = Container.instance.get(id.prototype.constructor);
+    Reflect.defineProperty(target, propertyKey, { value: injectable });
+  };
+};
